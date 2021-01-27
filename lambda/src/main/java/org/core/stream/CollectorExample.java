015da307310cafac3e74c2b736785fc1c5020452
Henry <dongyang1993@outlook.com>
2021-01-27 14:37:36 +0800

    commit

--- lambda/src/main/java/org/core/stream/CollectorExample.java
+++ lambda/src/main/java/org/core/stream/CollectorExample.java
@@ -1,4 +1,29 @@
 package org.core.stream;
 
-public class CollectorExample {
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CollectorExample extends DataBase {
+
+    @Test
+    public void test() {
+        String collect = employeeList.stream().map(Employee::getName).collect(Collectors.joining(";", "[", "]"));
+
+        List<String> list = employeeList.stream().map(Employee::getName).collect(Collectors.toList());
+
+        Set<String> set = employeeList.stream().map(Employee::getName).collect(Collectors.toSet());
+
+        ArrayList<String> arr = employeeList.stream().map(Employee::getName).collect(Collectors.toCollection(ArrayList::new));
+
+        Map<String, Integer> map = employeeList.stream().collect(Collectors.toMap(Employee::getName, Employee::getAge));
+
+        System.out.println("");
+
+    }
+
 }
